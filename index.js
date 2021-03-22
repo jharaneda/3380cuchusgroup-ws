7ac269e810511c52d13bede32dcc212003ca14f0
Jhonatan Araneda <jharanedac@gmail.com>
2021-03-21 22:15:49 -0700

    Added index.js file with its configurations

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = 8000;
+
+const corsOption = {
+  origin: "*",
+};
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const starFlow = require('./routes/shoppingRouteJar86.js')
+
+app.use('/tenantList', starFlow)
+app.use('/', starFlow)
+
+app.listen(port, () => {
+  console.log(`Web service is listening on ${port}`);
+});
